e0b16105f70ae56957e3948a7caf09ee96e3eb1d
Dave.Potter <dave@datonalabs.org>
2024-05-14 18:20:27 +0100

    make Session extend a new LoginSession class

--- src/model/Session.js
+++ src/model/Session.js
@@ -2,164 +2,63 @@
 // Distributed under the MIT software license, see the accompanying
 // file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-import * as assert from './utils/assertions';
-import { ecdsa } from '@bubble-protocol/crypto';
-import { Key } from '@bubble-protocol/crypto/src/ecdsa';
-import { stateManager } from '../state-manager';
-
-/**
- * Application state enum. @See the `state` property below.
- */
-const STATES = {
-  open: 'open',
-  loggedIn: 'logged-in'
-}
+import { LoginSession } from './utils/LoginSession';
 
 /**
  * A Session is an instance of the app with a locally saved state. The local device can support
  * multiple sessions allowing the user to have different accounts for different wallet accounts.
  * 
- * The Session is identified by its ID, passed in the constructor. The state is saved to 
- * localStorage with the Session ID as the key name.
+ * The `LoginSession` base class provides the basic session management functions, including
+ * loading and saving the session state to local storage and providing login and logout functions.
  * 
- * The Session is responsible for initialising any account data, including deploying any smart 
- * contracts and constructing any bubbles if they haven't been created before.
+ * Implement this class to manage any app-specific session data and functions.
  */
-export class Session {
-
-  /**
-   * Session current state
-   */
-  state = STATES.open;
-
-  /**
-   * The session's unique id derived from appId, chainId and account
-   */
-  id;
-
-  /**
-   * The wallet object
-   */
-  wallet;
-
-  /**
-   * The externally owned account address selected by the user's wallet
-   */
-  account;
-
-  /**
-   * The chain id selected by the user's wallet
-   */
-  chainId;
-
-  /**
-   * The session private key, optionally held in local storage and read on construction.
-   */
-  loginKey;
-
-  /**
-   * Constructs this Session from the locally saved state.
-   */
-  constructor(appId, wallet) {
-    console.trace('Constructing session');
-    assert.isNotEmpty(appId, 'appId');
-    assert.isObject(wallet, 'wallet');
-    this.wallet = wallet;
-    this.account = wallet.account;
-    this.chainId = wallet.getChain();
-    assert.isNotEmpty(this.chainId, 'wallet chain id');
-    assert.isNotEmpty(this.account, 'wallet account');
-    this.id = appId+'-'+this.chainId+'-'+this.account.slice(2).toLowerCase();
-    console.trace('session id:', this.id);
-    this._loadState();
-  }
-
-  /**
-   * Initialises the Session. The state of construction is determined by the properties read  
-   * from the local saved state during construction.
-   */
-  async initialise() {
-    console.trace('Initialising session');
-    // TODO: Add any app-specific session initialisation here
-    console.trace('Session initialised');
-    return this.getSessionData();
-  }
+export class Session extends LoginSession {
 
   /**
    * Returns an object containing the public data for this session
    */
   getSessionData() {
     return {
-      account: this.account,
-      chainId: this.chainId,
-      login: {
-        address: this.loginKey ? this.loginKey.address : undefined,
-        publicKey: this.loginKey ? '0x'+this.loginKey.cPublicKey : undefined
-      }
+      ...super.getSessionData(),
+      // TODO: Add any app-specific session data here
     }
   }
-
+  
   /**
-   * Logs in by requesting a login message signature from the user's wallet.
-   * If `rememberMe` is set, the login will be saved indefinitely, or until `logout` is called.
-   */
-  async login(rememberMe = false) {
-    if (this.state === STATES.loggedIn) return Promise.resolve();
-    return this.wallet.login(this.account)
-    .then(signature => {
-      this.loginKey = new Key(ecdsa.hash(signature));
-      if (rememberMe) this._saveState();
-      else this._calculateState();
-    })
+   * Called after a successful login.
+   */ 
+  async _onLogin() {
+    // TODO: Add any app-specific login tasks here
   }
 
   /**
-   * Logs out of the session, deleting any saved login details
-   */
-  logout() {
-    this.loginKey = undefined;
-    // TODO: Clear any app-specific session data on logout
-    this._saveState();
+   * Called when logging out and before saving the session state to localStorage.
+   */ 
+  async _onLogout() {
+    // TODO: Add any app-specific logout tasks here, such as clearing state data
   }
 
   /**
-   * Loads the Session state from localStorage
+   * Handle state data loaded from localStorage. Override this method to handle any app-specific
+   * session data.
+   * 
+   * @param {Object} stateData 
    */
-  _loadState() {
-    const stateJSON = window.localStorage.getItem(this.id);
-    const stateData = stateJSON ? JSON.parse(stateJSON) : {};
-    console.trace('loaded session state', stateData);
-    try {
-      this.loginKey = stateData.key ? new Key(stateData.key) : undefined;
-    }
-    catch(_){}
-    // TODO: load any app-specific session data
-    this._calculateState();
+  _parseLoadedStateData(stateData) {
+    // TODO: Parse any app-specific session data here
   }
-
+  
   /**
-   * Saves the Session state to localStorage
+   * Called when saving the session state to localStorage. Override this method to return any
+   * app-specific session data to be saved.
+   * 
+   * @returns {Object} Any app-specific state data to be saved to localStorage. Empty by default.
    */
-  _saveState() {
-    console.trace('saving session state');
-    const stateData = {
-      key: this.loginKey ? this.loginKey.privateKey : undefined
-      // TODO: add any app-specific session data to be saved
-  };
-    window.localStorage.setItem(this.id, JSON.stringify(stateData));
-    this._calculateState();
+  _getStateDataForSaving() {
+    return {
+      // TODO: Add any app-specific session data here
+    };
   }
-
-  /**
-   * Determines the value of this.state
-   */
-  _calculateState() {
-    const oldState = this.state;
-    this.state = this.loginKey ? STATES.loggedIn : STATES.open;
-    if (this.state !== oldState) {
-      console.trace("session state:", this.state);
-      stateManager.dispatch('session-state', this.state);
-    }
-  }
-
+    
 }
